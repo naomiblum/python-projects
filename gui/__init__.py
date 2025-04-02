08e8da6a4f94f44bdc0ead82af64cb42072fd158
Naomi Blum <naomiblum22@gmail.com>
2025-04-02 09:49:00 +0300

    Delete gui directory

--- gui/__init__.py
+++
@@ -1,9 +0,0 @@
-from .main_window import MainWindow
-from .utils import setup_theme
-
-# gui/__init__.py
-# Initialize the 'gui' package.
-
-
-# Perform any package-level initialization here
-setup_theme()
